3c1186aad22bfb54fc1cdb397711e8a4a0cb3253
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-06-05 23:16:50 +0800

    Update abvssv.go

--- internal/abvss/abvssv.go
+++ internal/abvss/abvssv.go
@@ -32,7 +32,7 @@ func (vss *ABVSS) VerifyLCM(lcm []*big.Int, index int) error {
 	vss.Count++
 	vss.mutex.Unlock()
 	fmt.Printf("node %v count: %d\n", vss.nodeid, vss.Count)
-	fmt.Printf("node %v ilist: %d\n", vss.nodeid, vss, vss.ilist)
+	fmt.Printf("node %v ilist: %d\n", vss.nodeid, vss.ilist)
 	if vss.Count == vss.nodenum-vss.degree && !vss.done {
 		//log.Printf("node %v verify", vss.nodeid)
 		xlist := make([]*big.Int, vss.nodenum-vss.degree)
