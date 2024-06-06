af012eaf88d98b45da30a501375d5deb8f94a2e6
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-06-07 02:21:55 +0800

    add

--- pkg/config/cmd/make.go
+++ pkg/config/cmd/make.go
@@ -4,8 +4,8 @@ import "github.com/QinYuuuu/abvss/pkg/config"
 
 func main() {
 	//config.ElgamalCurve25519KeyGen(128, "D:/mycode/gocode/src/abvss")
-	config.SigKeyGen(64, 21, "D:/mycode/gocode/src/abvss")
-	config.EncKeyGen(64, 21, "D:/mycode/gocode/src/abvss")
+	config.SigKeyGen(128, 42, "D:/mycode/gocode/src/abvss")
+	config.EncKeyGen(128, 42, "D:/mycode/gocode/src/abvss")
 	//config.LoadSigKey(4, "D:/mycode/gocode/src/abvss")
 	//ip1, ip2, ip3 := config.LoadIPList_Local(4, "D:/mycode/gocode/src/abvss")
 	//fmt.Println(ip1, ip2, ip3)
