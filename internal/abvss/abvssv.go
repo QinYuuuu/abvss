390290a8d95670f69353066d55e8a60270e6943c
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-06-06 11:25:42 +0800

    Update abvssv.go

--- internal/abvss/abvssv.go
+++ internal/abvss/abvssv.go
@@ -2,7 +2,6 @@ package abvss
 
 import (
 	"errors"
-	"fmt"
 	"github.com/QinYuuuu/abvss/crypto/utils/polynomial"
 	"math/big"
 )
@@ -32,10 +31,9 @@ func (vss *ABVSS) VerifyLCM(lcm []*big.Int, index int) error {
 	vss.Count++
 	vss.mutex.Unlock()
 	/*
-	fmt.Printf("node %v count: %d\n", vss.nodeid, vss.Count)
-	fmt.Printf("node %v ilist: %d\n", vss.nodeid, vss.ilist)
+		fmt.Printf("node %v count: %d\n", vss.nodeid, vss.Count)
+		fmt.Printf("node %v ilist: %d\n", vss.nodeid, vss.ilist)
 	*/
-	 */
 	if vss.Count == vss.nodenum-vss.degree && !vss.done {
 		//log.Printf("node %v verify", vss.nodeid)
 		xlist := make([]*big.Int, vss.nodenum-vss.degree)
