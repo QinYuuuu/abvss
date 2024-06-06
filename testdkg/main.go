11a26ae5ee500a21a488cf6f68696fff4b275aea
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-06-07 01:25:53 +0800

    Update main.go

--- testdkg/main.go
+++ testdkg/main.go
@@ -75,7 +75,7 @@ func test(n, batchsize, f, id int, str string, aws int) {
 	sk, pk := config.LoadSigKey(n, str)
 	//epk, evk, esks := party.EncKeyGen(uint32(n), uint32(f+1))
 	epk, evk, esks := config.LoadEncKey(n, str)
-	testNum := 1
+	testNum := f + 1
 	signature := make([][]byte, testNum)
 
 	for k := 0; k < testNum; k++ {
@@ -160,7 +160,7 @@ func TestDKG(id, n, f, batchsize, vnum int, pint *big.Int, pk1 []kyber.Point, sk
 			}
 		}(i)
 	}
-	for i := 0; i < n-f; i++ {
+	for i := 0; i < f+1; i++ {
 		go func(i int) {
 			for {
 				if node.Vss[i].Count >= n-f {
@@ -171,8 +171,8 @@ func TestDKG(id, n, f, batchsize, vnum int, pint *big.Int, pk1 []kyber.Point, sk
 		}(i)
 	}
 
-	wg.Add(n - f)
-	for i := 0; i < n-f; i++ {
+	wg.Add(f + 1)
+	for i := 0; i < f+1; i++ {
 		go func(i int) {
 			for {
 				if node.Osv[i].Done() {
