c750c4c3a75496363dee7eb60feb1eba544fdd97
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-09-02 04:19:11 +0800

    test2

--- pkg/config/cmd/make.go
+++ pkg/config/cmd/make.go
@@ -11,6 +11,6 @@ func main() {
 	//fmt.Println(ip1, ip2, ip3)
 	//config.GenerateIPList_Local(4, "D:/mycode/gocode/src/abvss")
 	//config.LoadIPList_Local(4, "D:/mycode/gocode/src/abvss")
-	config.LoadRawIP_2(1, 4, "D:/mycode/gocode/src/abvss")
-	config.SHRawIP(1)
+	config.LoadRawIP_2(2, 4, "D:/mycode/gocode/src/abvss")
+	config.SHRawIP(2)
 }
